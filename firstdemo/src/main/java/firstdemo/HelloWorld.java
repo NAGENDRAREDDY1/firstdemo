a408b42c0a3ed07729412c809640e24501c17c3c
Siva nagendra reddy <Siva nagendra reddy@DESKTOP-A8Q3SC2>
2021-08-16 13:58:04 +0530

    First commit!

---
+++ firstdemo/src/main/java/firstdemo/HelloWorld.java
@@ -0,0 +1,11 @@
+package firstdemo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("welcome to git");
+
+	}
+
+}
