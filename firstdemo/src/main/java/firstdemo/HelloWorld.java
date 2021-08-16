42cad5936f3ffe2a482fc3febd80d0cacf62be0a
Siva nagendra reddy <Siva nagendra reddy@DESKTOP-A8Q3SC2>
2021-08-16 16:51:13 +0530

    commit all

--- firstdemo/src/main/java/firstdemo/HelloWorld.java
+++ firstdemo/src/main/java/firstdemo/HelloWorld.java
@@ -7,5 +7,5 @@ public class HelloWorld {
 		System.out.println("welcome to git");
 
 	}
-
-}
+   
+    }
---
+++ firstdemo/src/main/java/firstdemo/sampleapp.java
@@ -0,0 +1,14 @@
+package firstdemo;
+
+public class sampleapp {
+
+	public void display() {
+		int[] arr= {23,45,56,78,6};
+		for (int num:arr)
+			System.out.println(num);
+	}
+    public void print() {
+    	System.out.println("Print All Array number:");
+    	display();
+    }
+}
